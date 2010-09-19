55f145eadf9de816fcbaec88507b0a6a67146c08
George Marshall <george@cukerinteractive.com>
2010-09-18 20:26:15 -0700

    Added random_photos template tag

--- photologue/templatetags/photologue_tags.py
+++ photologue/templatetags/photologue_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from photologue.models import Photo
+
 register = template.Library()
 
 @register.inclusion_tag('photologue/tags/next_in_gallery.html')
@@ -9,3 +11,10 @@ def next_in_gallery(photo, gallery):
 @register.inclusion_tag('photologue/tags/prev_in_gallery.html')
 def previous_in_gallery(photo, gallery):
     return {'photo': photo.get_previous_in_gallery(gallery)}
+
+@register.inclusion_tag('photologue/tags/gallery_photo.html')
+def random_photos(number=0, is_public=True):
+    """
+    Return a specified number of random photos from all galleries
+    """
+    return {'object_list': Photo.objects.sample(number, is_public)}
