f870c764a31a37258a8ec846e027961c6659189b
George Marshall <george@cukerinteractive.com>
2010-09-19 04:37:19 -0700

    Some view refactoring to make things a bit cleaner

--- photologue/views.py
+++ photologue/views.py
@@ -7,26 +7,37 @@ from forms import AjaxRequestForm
 from models import Photo
 from urls import photo_args
 
-def photo_index(request):
-    # Handle AJAX request
-    if request.is_ajax():
-        ajax_request = AjaxRequestForm(data=request.GET)
-        if ajax_request.is_valid():
-            request_type = ajax_request.cleaned_data['type']
+def ajax_view(request):
+    """
+    Handle any async JS server function request
+    """
+    def JsonResponse(data):
+        """
+        Wraper to allow an easy method to return json data
+        """
+        return HttpResponse(json.dumps(data), mimetype='application/json')
 
-            # sample()
-            if request_type == 'sample':
-                # Excluded is_public for obvious security reasons
-                ajax_request.fields['count'] = forms.IntegerField(min_value=0, max_value=100, required=False)
-                ajax_request.full_clean()
-                if ajax_request.is_valid():
-                    photo_list = []
-                    photos = Photo.objects.sample(ajax_request.cleaned_data['count'])
-                    for photo in photos:
-                        photo_list.append({
-                            'thumbnail': photo.get_thumbnail_url(),
-                            'title': photo.title,
-                            'url': photo.get_absolute_url()
-                        })
-                    return HttpResponse(json.dumps(photo_list), mimetype='application/json')
+    ajax_request = AjaxRequestForm(data=request.GET)
+    if ajax_request.is_valid():
+        request_type = ajax_request.cleaned_data['type']
+
+        # sample()
+        if request_type == 'sample':
+            # Excluded is_public for obvious security reasons
+            ajax_request.fields['count'] = forms.IntegerField(min_value=0, max_value=100, required=False)
+            ajax_request.full_clean()
+            if ajax_request.is_valid():
+                photo_list = []
+                photos = Photo.objects.sample(ajax_request.cleaned_data['count'])
+                for photo in photos:
+                    photo_list.append({
+                        'thumbnail': photo.get_thumbnail_url(),
+                        'title': photo.title,
+                        'url': photo.get_absolute_url()
+                    })
+                return JsonResponse(photo_list)
+
+def photo_index(request):
+    if request.is_ajax():
+        return ajax_view(request)
     return archive_index(request, **photo_args)
